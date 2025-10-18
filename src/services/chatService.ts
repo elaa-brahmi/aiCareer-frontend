5f0f6bd913b7714c6fbed6ae2ada5d95fbddae4f
elaa <elaa.brahmii@gmail.com>
2025-10-18 23:47:12 +0100

    add chat service

---
+++ src/services/chatService.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest } from 'next'
+
+import { getAxiosInstance } from '@/lib/api/axiosConfig.ts'
+import { ApiErrorType } from '@/lib/api/constants'
+import { ApiError } from '@/lib/api/errors'
+
+export const generatechatBotResponse = async (userInput:string, serverRequest?: NextApiRequest) => {
+  try {
+    const axios = await getAxiosInstance(serverRequest,true);
+    const response = await axios.post(`/api/chat/embed`, {msg:userInput} );
+    return response.data
+  } catch (error) {
+    if (error instanceof ApiError) {
+      throw error
+    }   
+    throw new ApiError(ApiErrorType.SERVER, 'Failed to delete resume', undefined, error)
+  }
+} 
